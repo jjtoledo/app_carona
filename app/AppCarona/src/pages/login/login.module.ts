a32acbee22f00d6404f5def6f9c583430518961f
guilhermesfelix <guilhermesfelix@gmail.com>
2017-04-05 22:21:28 -0300

    ionic inicial

---
+++ app/AppCarona/src/pages/login/login.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { IonicModule } from 'ionic-angular';
+import { Login } from './login';
+
+@NgModule({
+  declarations: [
+    Login,
+  ],
+  imports: [
+    IonicModule.forRoot(Login),
+  ],
+  exports: [
+    Login
+  ]
+})
+
+export class LoginModule {}
